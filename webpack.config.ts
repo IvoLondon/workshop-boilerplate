5f5f2e8e4f16245d55f86eff051b52758dd0a3f0
Ivo Iliev <ivo.iliev@vodafone.com>
2021-12-01 21:53:07 +0000

    feat: implement webpack analyser

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,24 +1,26 @@
-import path from 'path';
+import path from "path";
+import * as Analyser from "webpack-bundle-analyzer";
 
 export default {
-    entry: path.resolve(__dirname, 'src/index.ts'),
-    devtool: 'source-map',
-    output: {
-        clean: true,
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.[hash].js',
-        libraryTarget: 'umd'
-    },
-    resolve: {
-        extensions: ['.ts'],
-    }, 
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                include: path.resolve(__dirname, 'src'),
-            }
-        ]
-    }
+  entry: path.resolve(__dirname, "src/index.ts"),
+  devtool: "source-map",
+  output: {
+    clean: true,
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.[hash].js",
+    libraryTarget: "umd",
+  },
+  resolve: {
+    extensions: [".ts"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        include: path.resolve(__dirname, "src"),
+      },
+    ],
+  },
+  plugins: [new Analyser.BundleAnalyzerPlugin()],
 };
